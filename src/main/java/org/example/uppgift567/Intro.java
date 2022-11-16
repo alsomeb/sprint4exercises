10fdf971374f88ae755319971fdbe1ff3101e9c7
alsomeb <alsomeb@gmail.com>
2022-11-16 13:19:54 +0100

    Exercise 5, 6 ,7 refactoring

--- src/main/java/org/example/uppgift567/Intro.java
+++ src/main/java/org/example/uppgift567/Intro.java
@@ -2,5 +2,6 @@ package org.example.uppgift567;
 
 import java.io.Serializable;
 
+// Kan va Record, pga final boolean samt getter
 public record Intro(boolean isConnected) implements Serializable {
 }
--- src/main/java/org/example/uppgift567/Response.java
+++ src/main/java/org/example/uppgift567/Response.java
@@ -2,5 +2,6 @@ package org.example.uppgift567;
 
 import java.io.Serializable;
 
+// Kan va Record, pga finala variablar samt getters bara!
 public record Response(boolean isFound, User foundUser, String errorMsg) implements Serializable {
 }
